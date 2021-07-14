8247bd58a826baffced481e70982c6906e48ddd9
Sohyun <park.sohyun0430@gmail.com>
2021-07-14 11:36:33 +0900

    last day

--- app.cpp
+++ app.cpp
@@ -1,5 +1,12 @@
 #include<iostream>
 using namespace std;
+class Human {
+private:
+	string name;
+	int age;
+public:
+
+};
 int main()
 {
 	cout << "Im fine" << endl;
--- prac.cpp
+++ prac.cpp
@@ -4,5 +4,6 @@ int main()
 {
 	cout << "Hello!" << endl;
 	cout << "I'm done!" << endl;
+	cout << "TOday is last dat" << endl;
 	return 0;
 }
