add9963f2046e4072270df451cf8c02b8191b1b8
Sohyun <park.sohyun0430@gmail.com>
2021-07-14 11:43:41 +0900

    human

--- app.cpp
+++ app.cpp
@@ -1,13 +1,41 @@
 #include<iostream>
+#include<string>
 using namespace std;
 class Human {
-private:
+protected:
 	string name;
 	int age;
 public:
-
+	Human(string& name,int& age):name(name),age(age){}
+	virtual void info() const {
+		cout << "이름 : " << name << endl;
+		cout << "나이 : " << age << endl;
+	}
+};
+class Student : public Human {
+protected:
+	int id;
+public:
+	Student(string& name, int& age, int& id) : Human(name, age) { this->id = id; }
+	void info() const {
+		cout << "학생 이름 : " << name << " 학생 id : " << id << " 학생 나이 : " << age << endl;
+	}
+	void study() const {
+		cout << "여러가지를 배워봅시다.ㅇ.ㅇ..ㅇ아아ㅏㅇ" << endl;
+	}
+};
+class Prof : public Human {
+protected:
+	string depart;
+public:
+	Prof(string& name, int& age, string& depa) :Human(name, age) { depart = depa; }
+	void info() const {
+		cout << "교수 이름 : " << name << " 교수 나이 : " << age << "  교수 학과 : " << depart;
+	}
+	void talk() const {
+		cout << "한 잔 하겠나!" << endl;
+	}
 };
 int main()
 {
-	cout << "Im fine" << endl;
 }
